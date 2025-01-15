75fa1f20d76c42da3017b13c0185c7a3d07b7f11
axadov070 <axadov712@gmail.com>
2025-01-15 12:15:08 +0500

    2024 dars larim

---
+++ axadov.py
@@ -0,0 +1,1111 @@
+# def yosh_top(ism, t_yil):
+# def yosh_top(ism, t_yil):
+#     print(f"Assalomu Aleykum {ism.title()}, siz {t_yil}-da tugilgan siz va yoshingiz {2024-t_yil} da.")
+
+# yosh_top("abdurauf", t_yil=2009)
+# print("hello world !")
+# print("hayrli tong !")
+# print(2+4*2)
+# print(19/5)
+# print(2**4)
+# print("odamni ersang , demagil odamni, \ n0niki xalq gamidin gami")
+# ism = "abdulloh"
+# yosh = 25
+# print(ism)
+# print(yosh)
+# ism = "abdulloh"
+# print (ism)
+# ism = "muhammad"
+# print(ism)
+# print("hello world")
+# radius  = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananig yuzi=", aylana_yuzi)
+# sonlar = [0, -5, -9, -12, 99]
+# def tartibla(qiymat):
+#     qiymat.sort()
+#     print(qiymat)
+
+#     tartibla(sonlar)
+# print(5**4)
+# print(22/4)
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+# matn = "men yangi tel ooldim"
+# print (matn)
+# ism = "Ahad"
+# familiya = 'Qayum'
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif)
+# ism = "abdurauf"
+# familiya = 'axadov ✔'
+# ism_sharif = f"{ism}   {familiya}"
+# print(ism_sharif)
+# ism = input("ismingiz nima \n>>>")
+# print("assalomu aleykum ," + ism.title())
+# kocha = "Bog'bon"
+# mahalla = "Sog'bon"
+# tuman = "Bodomzor"
+# viloyat = "Samarqand"
+
+# # Yuqoridagio'zgaruvchilarni jamlab, quyidagi ko'rinishda konsolga chiqaring:
+# # Diqqat uzun kodlarni \ belgisi yordamida keyingi qatorga
+# # ko'chirish mumkin
+# print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+#       tuman + " tumani, " + viloyat + " viloyati")
+# kocha = "bahoriston"
+# mahalla = "yangi zamin "
+# tuman = " yangi namangan tumani "
+# viloyat = "shahar"
+# print(kocha + " ko'chasi, " + mahalla + " mfy, " + \
+#       tuman + " " + viloyat + " ")
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalomu alaykum!")
+# def salom_ber():
+# """"salom beruvchi funksiya"""
+# print("assalomu aleykum !")
+# def salom_ber(ism):
+#     """Fodyalanuvchi ismini qabul qilib, 
+#         unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+# ism = "abdurauf"
+# familiya = 'axadov ✔'
+# ism_sharif = f"{ism}   {familiya}"
+# print(ism_sharif)
+# def salom_ber(ism):
+#     """"foydalanuvchi ismini qabul qili unga salom beruvchi funksiya"""
+#     print(f"assalomu aleykum, xurmatli {ism.title()}!")
+# ism = "Abdurauf ✔"
+# print(ism)
+# ism = ("abdurauf yaxshi bola emas chunki u juda kop kochada yuradi !!!!!!✔ ")
+# # print(ism)
+""""1-chi mashiq"""
+# print("Ahmadning `yuvvosh` mushugi meni ko'rsa, doim yugurib keladi")
+"""2-chi mashq"""
+"""1-chi usul"""
+# print(93343+94903-22344+7363)
+"""2-chi usul""" 
+# a = 93343
+# s = 94903 
+# g = 22344
+# b = 7363 
+# S = a+s-g+b
+# print(S)
+
+# a = 2009
+# b = 4422
+# s = 873578328752389325892352389598359357
+# c = (a+b//s)
+# print(c)
+# ism = "Ahadov"
+# familiya = 'Abdurauf'
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif)
+
+# print('Abdurauf Axadovv'.upper())
+
+# kocha="Bog'bon"
+
+# mahalla="Sog'bon"
+
+# tuman="Bodomzor" 
+
+# viloyat="Samarqand"
+# print(kocha, mahalla, tuman, viloyat)
+
+# a = 20  # Sonlar musbat yoko
+# b = -30 # manfiy bo'lishi mumkin
+# c = a + b
+# print(c)
+
+# # Kvadratning yuzini hisoblaymiz
+# kvdrt_tmni = 20 # Kavdratning tomoni 20 ga teng
+# kvdrt_yuzi = kvdrt_tmni**2 # Kvadrat yuzini hisoblaymiz
+# print(kvdrt_yuzi)
+
+# a = -20
+# b = 40
+# c = b//a
+# print(c) # natija o'nlik son bo'lad
+
+# a = 30
+# b = 20
+# # Quyidagi arifmetik amallarning natijasi o'nlik son hosil qiladi
+# print(a+b) 
+# print(a*b)
+# print(a**b)
+# print(2*(a+b))
+
+
+
+# ism = 'Abdurauf'
+# yosh = 16 
+# xabar = ism + ' siz ' + str(yosh) + ' yoshdasiz ✔'
+# print(xabar)
+
+# t_yil = int(input("tugilgan yilingizni kiriting :"))
+# yosh = 2024 - t_yil
+# print(" siz " + str(yosh) + " yoshda ekansiz")
+
+
+# balansizgiz = "1,000,000,000,000,000,000,000,000,000,000,000 $"
+# print("siz ning o'rtacha balansingiz")
+# print(balansizgiz) 
+
+# kocha = "Go'zal"
+# tuman = "Yangi namangan"
+# shahar = "Namangan"
+
+# print(kocha + " ko'chasi, " + \
+#       tuman + " tumani, " + shahar + " shahri") 
+# ism ="anvar"
+"""10-dekabr"""
+# print(ism)
+# print("☠  ☁")
+# ism = 'abdurauf'
+# print("mening ismim " + ism)
+# ism = 'jonibek'
+# familiya = 'joqiyev'
+# print(ism + ' '  + familiya)
+
+# ism = 'Abdurauf'
+# familiya = 'Axadov'
+# laqabi  = 'qorixon'
+# ism_sharif  = f"{ism} {familiya} {laqabi}"
+# print(ism_sharif)
+# ism = 'jonibek'
+# familiya  = 'axadov'
+# print(f"Salom, mening ismim {familiya} {familiya}")
+
+# ism = 'عبد الرؤوف'
+# familiya  = 'أخادوف'
+# print(f"salom, mening ismim {ism} {familiya}")
+
+# ism  = "mahdiy"
+# familiya = "jabborov"
+# print(f"salom , mening ismim va fimiliyam {ism} {familiya}")
+
+
+# ism = 'Abdurauf'
+# familiya = 'Axadov'
+# print(f"salom, mening ismim {ism}  {familiya}  !")
+
+
+
+# print('hello world ! ')
+# print('hello \tworld')
+# print('hello \nworld')
+
+
+
+"""" 5-dars  amaliyot """
+
+# kocha ="Go'zal"
+# mahalla ="Yangi zamin"
+# tuman = "Yangi namangan"
+# viloyat = "Namangan shahri"
+
+# print(f"{kocha} kochasi , {mahalla} mahallasi , {tuman} tumani , {viloyat}")
+
+
+# ism = input("Ismingiz nima?")
+# print("Assalom alaykum, " + ism)
+
+# viloyat = input("Viloyatingiz: " )
+# tuman = input("Qaysi tumanda yashaysiz: ")
+# mfy = input("Qaysi mahhalada yashaysiz")
+# print(f"siz {viloyat} viloyati, {tuman} tumani, {mfy} mahallasida yashaysiz")
+# 
+# 
+# 
+
+
+
+# a = 2443434343
+# b = 234444343
+# c = a+b
+# print(c)
+
+
+# a = -20
+# b = 40
+# c = b//a
+# print(c) 
+
+
+
+# a = 2
+# b = 3.0
+# print(a+b) 
+# print(a*b)
+# print(a**b)
+# print(2*(a+b))
+
+# print("boychechak".upper())
+# print("BOYCHECHAK".lower())
+# print("boychechak".title())
+# print("boychechak".capitalize())
+
+
+
+
+
+
+# """ nazorat ishi vazifa !!!"""
+
+
+# """1-mashq"""
+
+# ism, familiya, yosh = "ALi", "Olimov", 23
+
+
+
+# print(ism, familiya, yosh)
+
+# """ 2-mashq"""
+
+# print("Ahamdning `yuvvosh` mushugi meni ko'rsa doim yugurib keladi ! ")
+
+
+# """ 3-mashq """
+
+# a = 93343
+# b = 94903
+# d = 1560
+# s = a-b+d
+# print(s)
+
+# ism = input("ismizni kiriting")
+# familiya = input("familyezni kiriting")
+# yosh= input("yoshizni kiriting")
+# print(f"Assalomu Aleykum, {ism} {familiya}, Siz {yosh}-yoshdadasiz ! ")
+
+
+
+
+
+
+
+
+""" haftalik test """ 
+ 
+
+""" 1-mashq """
+
+
+# ism = "     DiLSHoda     "
+# print(ism.lstrip().upper())
+# print(ism.strip())
+# print(ism.title())
+# print(ism.upper())
+# print(ism.split())
+
+"""2-mashq"""
+
+
+# ism = input("ismizni kiriting")
+# maktab = input("maktabizni kiriting")
+# fan1 = input("1-chi soatdagi fanizni kiriting ")
+# fan2 = input("2-chi soatdagi fanizni kiriting ") 
+# fan3 = input("3-chi soatdagi fanizni kiriting ")
+# fan4 = input("4-chi soatdagi fanizni kiriting ")
+# print(f"{ism}ning {maktab}-sonli maktabida quyidagi fanlarni o'qiydi:1-chi soat - {fan1}, 2-chi soat - {fan2}, 3-chi soat - {fan3}, 4-chi soat - {fan4}")
+
+
+"""3-mashq"""
+
+
+# ism = input("ismizni kiriting: ")
+# yosh = input("yoshizni kiriting: ")
+# print(f"Mening ismim {ism.title()}, yoshim {yosh} da ")
+
+
+
+"""4-mashq""" 
+
+
+# print("mening `MAX`nomli mushugim bor")
+# print("Salimning asablari doimgidek zo `r")
+# ismfamilya = 'Azizbek Abdugafforov'
+# print(ismfamilya)
+# meros = "50000 dollar"
+# print(meros)
+# ism_familiya = "jafforov ali"
+# print(ism_familiya)
+
+
+""" yangi test """
+
+
+
+"""1-mashq """
+# ism = input("ismizni kiriting: ")
+# familiya = input("familyangizni kiriting: ")
+# ota_ism = input("otangizni ismini kiriting: ")
+# ota_familya = input("otezni familyasini kiriting: ")
+
+# print(f"Assalomu aleykum ! {ism.title()} {familiya.title()}, sizni otangizni ism familyasi  {ota_ism.title()}, {ota_familya.title()} ")
+
+
+
+"""2-chi mashq"""
+
+# ism1 = "Ali"
+# ism2 = "Ahmad"
+# ism3 = "Anvar"
+# print(f"{ism1} {ism2} {ism3}")
+
+
+
+"""3_mashq"""
+
+
+# a = 5784
+# b = 847
+# d = 3234
+# c = (a+b-d)
+# print(c)
+
+
+"""4-mashq"""
+
+# maktab = input  ("maktabizni kiriting")
+# bogcha = input ("ukangizni bogchasini kiriting")
+# print(f"maktabingiz {maktab}, ukangizni bogchasi {bogcha}")
+
+
+
+
+"""5-mashq"""
+
+# yosh = input ("yoshizni kiriting")
+# manzil = input ("yashash manzilizni kriting")
+# print(f"sizni yoshiz = {yosh}, yashash manziliz esa = {manzil} ")
+
+
+
+
+# ism = input("ismizni kiriting")
+# t_yil = int(input("tugilgan yilizni kiriting !"))
+# yosh = 2024 - t_yil
+# print("Mening ismim" + " " + (ism) +  ' ' + str(yosh) + " yoshdaman")
+
+# ism = input("ismizni kiriting")
+# t_yil = int(input("tugilgan yilizni kiriting"))
+# yosh = 2024 - t_yil
+# print("mening ismim "  +  (ism)         + str(yosh) + " yoshdaman")  
+
+# kvdrt_tmni = 9
+# kvdrt_yuzi = kvdrt_tmni**8
+# print(kvdrt_yuzi)
+
+
+# pi = 3.14159 # o'nlik son (float)
+# radius = 10  # butun son (integer)
+# diametr = 2*radius
+# print("Aylana uzunligi ", pi*diametr, " ga teng.")
+
+
+
+
+# a = -20
+# b = 40
+# c = b/a
+# print(c) 
+
+
+# a = -20
+# b = 40
+# c = b/a
+# print(c) # natija o'nlik son bo'ladi
+
+# a = 44
+# b = 14
+# c = a*b
+# print(c)
+
+# pi = 3 
+# radius = 21
+# diametr = 2*radius
+# print("Aylana uzunligi", pi*diametr,)
+
+
+
+
+# a = 3131331
+# b = 64344
+# c = a+b
+# print(c)
+
+
+
+# ism = 'Jobir'
+# yosh = 36
+# xabar = ism + ' ' + str(yosh) + ' yoshda'
+# print(xabar)
+
+
+
+
+
+# ism = 'Abdurauf'
+# yosh = 15
+# xabar = ism + ' ' + str(yosh) + ' yoshda'
+# print(xabar)
+
+
+
+# ism = 'Jobir'
+# yosh = 36
+# print(type(ism))  # ism degan o'zgaruvchining turini konsolga chiqaramiz
+# print(type(yosh))
+
+
+# ism = 'Abdurauf'
+# yosh = 15
+# print(type(ism))
+# print(type(yosh))
+
+
+# t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# yosh = 2024 - t_yil 
+# print("Siz " + str(yosh) + " yoshda ekansiz")
+
+
+# ism = input("Assalomu aleykum ismizni kiriting: ").title()
+# t_yil = int(input(f"{ism} tug'ilgan yilizni kiriting: "))
+# yosh = 2024 - t_yil
+# print(f"{ism} siz " + str(yosh) + " yoshda ekansiz")
+
+
+
+
+
+
+# x = int(input("Istalgan son kiriting:\n>>>"))
+# print(x, " ning kvadrati ", x**2, " ga teng")
+# print(x, " ning kubi ", x**3, " ga teng")
+
+
+
+
+# t_yil = input("Tug'ilgan yilingizni kiriting: ")
+
+# t_yil = int(t_yil)
+
+# yosh = 2024 - t_yil # 
+
+# print("Siz " + str(yosh) + " yoshda ekansiz")
+
+
+
+
+
+# ism = input("Assalomu aleykum ismizni kiriting: ").title()
+# t_yil = int(input(f"{ism} tug'ilgan yilizni kiriting: "))
+# yosh = 2024 - t_yil
+# print(f"{ism} siz " + str(yosh) + " yoshda ekansiz")
+
+
+# ism = input("Sommekum ismizni kiriting: ").title()
+# t_yil = int(input(f"{ism} Tug'ilgan yilizni kiriting: "))
+# yosh = 2024 - t_yil
+# print(f"{ism} siz " + str(yosh) + " yoshda ekansiz")
+
+
+
+
+# ism = input("brat ismizni kiriting:>>>")
+# t_yil = input(f" {ism} yoshizni kiriting: ")
+# t_yil = int(t_yil)
+# yosh = 2024 - t_yil
+# print(f" {ism} {yosh}  yilda tugilgan ekansiz ! ")
+
+# son1 = float(input("Assalomu aleykum birinchi sonni kiriting: "))
+# son2 = float(input("endi ikkinchi sonni kiriting: "))
+# kvadrat = son1  ** 2
+# kubi = son1 ** 3 
+# kvadrat2 = son2 ** 2
+# kubi2 = son2 ** 3
+# print(f"Assalomu aleykum birinchi  sonni kvadrati : {kvadrat} kubi esa {kubi} , \nikkinchi kiritgan \
+#       soningiz : kvadrati {kvadrat2} kubi esa {kubi2} ga teng !")
+
+
+
+# son1 = float(input("Assalamu aleykum  bitta son kiriting men sizga u sonni kvadrat va kubini va hammasini topib beraman: "))
+# son2 = float(input("endi ikkinchi sonni kiriting: "))
+# kopaytirsh = son1 * son2 
+# bolsih = son1 / son2
+# orta_arifmwetik = son1 + son2 / 2 
+# kvdrt = son1 ** 2
+# kubi = son1 **3
+# kvdrt2 = son2 ** 2
+# kubi2 = son2 ** 3 
+
+
+# print(f"Assalamu aleykum siz \nkiritgan sonni kopaytmasi {kopaytirsh}\
+#  bolinmasi esa = {bolsih}\
+#  orta arifmetigi = {orta_arifmwetik} \
+# {son1} kvadrati esa {kvdrt}\
+# {son1}  kubi esa {kubi} ga teng\
+# {son2} ni kvadrati {kvdrt2} ga\
+# {son2} kubi esa {kubi2} ga teng ! Solomat boling ")\
+
+
+
+# uzb_axoli = input("Assalamu aleykum qaysi davlada yashaysiz ")
+# axoli_son = "38-mln"
+
+# print(f"salom siz {uzb_axoli} da yashaysiz va {uzb_axoli} da {axoli_son} odam  bor")
+
+
+
+
+
+
+
+# kvdrt_tmni = 20 
+# lvdrt_yzu = kvdrt_tmni**2
+# print(lvdrt_yzu)
+
+
+
+
+
+
+
+
+
+# pi = 3.14159 # o'nlik son (float)
+# radius = 10  # butun son (integer)
+# diametr = 2*radius
+# print("Aylana uzunligi ", pi*diametr, " ga teng.")
+
+# pi = 3.43223 
+# radius = 10 
+# diametr = 2*radius
+# print("aylana uzunligi", pi*diametr, "ga teng")
+
+
+# """amaliy ish !"""
+# son1 = float(input("assalomu aleykum bitta son kiriting "))
+# kvddrt = son1
+
+
+
+
+
+
+
+
+"""  1-test  """
+
+
+
+
+
+"""1-	Mashq | 10 Ball
+Ism, familiya, otasining ismi , yosh va sinfi deb nomlangan o’zgaruvchilar yarating va ularni print() funksuyasi yordamida konsulga chiqaring.
+"""
+
+# ism = 'amirxon'
+# familiya = 'axmadov'
+# otasi_ismi = 'qahhor'
+# otasi_familioas = 'joqiyev'
+# print(f"salom {ism} {familiya}, siz {otasi_ismi} {otasi_familioas} o'gilisiz !")
+
+"""2-	Mashq | 10 Ball
+Son va matn deb nomlangan o’zgaruvchilar yarating va ularni kommenga oling.
+"""
+
+# son = 542353254
+# matn = "assalomu aleykum"
+
+# print(F"{son} {matn}")
+
+"""3-	Mashq | 10 Ball
+265 ni 5 ga bo’ling chiqqan natijani 2 ga ko’paytiring va 751 ni 6 ga bo’lgandagi natijaga qo’shing.
+"""
+
+# a = 265 
+# b = 5
+# s = 2
+# aa = 751
+# bb = 6
+
+
+# c= a // b * s + aa // bb
+# print(c)
+
+
+"""
+4-	Mashq | 10 Ball
+8934 ni 34 ga ko’paytib hosil bo’lgan natijani, 62 va 47 ning ko’paytmasiga bo’ling.
+"""
+
+# a = 8934 
+# b = 34
+# f = 62
+# h = 47
+# c = a * b // f * h
+# print(c)
+
+
+
+"""5-	Mashq | 10 Ball
+Otam, onam, ukam, singlim va do’stim deb nomlangan o’zgaruvchilar yarating va ularning qiymatini konsulga chiqaring.
+"""
+
+
+# otam = 'ahmad'
+# onam = 'robiya'
+# ukam = 'mahdiy'
+# singlim = 'roziya'
+# print(f" {otam.title()}, {onam.title()}, {ukam.title()}, {singlim.title()}.")
+
+
+
+
+
+
+""" 2- test  """
+
+
+"""
+1-	Mashq | 5 Ball | Ushbu matinni konsulga chiqaring:
+“Ahmadning “yuvvosh” mushugi meni ko’rsa, doim yugurib keladi.”
+
+"""
+
+# print("Ahmadni `yuvvosh` mushugi meni ko`rsa doim yugurib keladi !")
+
+
+"""2-	Mashq | 10 Ball
+93434 ga 94903 ni qoshing, hosil bo’lgan natijadaan 22344 ni ayring va unga 7363 ni qo’shing
+"""
+
+# a = 93434
+# b = 94903 
+# s = 22344
+# g = 7363
+# c = a + b - s + g
+# print(c)
+
+
+
+
+"""3-	Mashq | 10 Ball
+Quidagi o’zgaruvchini ---> gul = “  BoyCHeCHak  ” 
+a.	Chap tomonidagi bo’shliqni olib tashlab;
+b.	Har ikki tomonidagi bo’shliqni olib tashlab;
+c.	Birinchi harifni katta qilib;
+d.	Barcha hariflarini katta qilib;
+e.	Barcha hariflarini kichik qilib konsulga chiqaring.
+"""
+
+# a = '   BoyCHeCHaK  '  
+# b = '   BoyCHeCHaK  '
+# d = '   BoyCHeCHaK  '
+# g = '   BoyCHeCHaK  '
+# h = '   BoyCHeCHaK  '
+# print(f"{a.capitalize()}, {b.lower()}, {d.title()}, {g.upper()}, {h.
+# strip() }")
+
+
+""" 4 - mashq Foydalanuvchidan u qaysi mamlakada yashashini so’rang va natijani barcha harflarini katta qilib konsulga chiqaring."""
+
+
+# davlat = input("salom qaysi mamlakatda yashaysiz: ")
+# print(F"Assalomu aleykum siz {davlat.upper()} da yashaysiz !")
+
+
+"""5-	Mashq | 15 Ball
+Ism, yosh, manzil, maktab, sinf deb nomlangan o’zgaruvchilar yarating va ularning qiymatini foydalanuvchidan so’rang. Keyin esa f”” yordamida namunadagi kabi matnni chiqaring.
+Namuna: Salom mening ismim Nigora, yoshim 23 da. Namanganda yashayman va BM maktabida o’qiyman.
+"""
+
+
+# ism = input("salom ismizni kiriting: ")
+# familiya = input("familyezni kiriting: ") 
+# yosh = input("yoshizni kiriting: ")
+# manzil = input("manzilizni kiriting: ")
+# maktab = input("maktabizni kiriting: ")
+# sinf = input("Endi esa sinfizni kiriting: ")
+# print(f"Salom mening ismim {ism.title()} {familiya.title()} yoshim {yosh} da. men {manzil} da  yashayman va {maktab} da oqiyman {sinf} sinfida oqiyman ")
+
+
+# ism = input("salom ismizni kiriting: ")
+# yosh = int(input("yoshizni kiriting: "))
+# T_yil = 2024 - yosh
+# print(f"salom mening ismim {ism } men {yosh} daman va {T_yil} yilda tugilganman !")
+
+
+# son1 = int(input("birinchi son  kiriting : "))
+# son2 = int(input("ikkinchi  son kriting" ))
+# from random import randrange
+
+
+# print(randrange(son1, son2)), 
+
+
+# import math
+
+# number = 3163
+# root = math.sqrt(number)
+# print(root) 
+
+# import math
+# number = 225
+# print(math.sqrt(number))  
+
+
+# import math 
+
+# print(math.sqrt(int(input("salon son kiritn :"))))
+
+
+
+# """class"""
+# """oop object programming"""
+
+# class Car():
+#     """Doctring"""
+
+
+#     def __init__(self, company:str, model:str, color:str, price:int,max_speed:int,year:int): #parametr
+#        self.company = company #hususiyat
+#        self.model = model
+#        self.color = color
+#        self.price = price
+#        self.max_speed = max_speed
+#        self.year = year
+
+
+
+
+
+
+
+#     def get_info(self):
+#       """mashina haqida maluimot beruvchi funksiya"""
+
+
+
+#       info = f"mashina haqdagi malumotlar: \nKampainya: {self.company} \nmodel: {self.model} \nRangi: {self.color}\nNarxi: {self.price} $ \nEng yuqori tezligi: {self.max_speed} \nIshlab chiqarilgan yili: {self.year} \n"
+#       return info
+# car2 = car("Tesla", "Model 3 ", "Black" , )
+
+
+
+
+# car1 = Car("Tesla", "model 3","black", "55_000$", 300, 2025  )         
+
+# yosh = int(input("Salom yoshgizni kiritng: "))
+# print(f"salom siz {yosh} yoshdasiz !")
+# print(type(int))
+
+# son1 = int(input("salom son kiritng: "))
+# son2 = int(input("ikinici sonni kiritng: "))
+# import random
+# print(random.randrange(son1,son2))
+
+
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# print("Birinchi meva: ", mevalar[0])
+# print("Ikkinchi meva: ", mevalar[1])
+
+
+
+
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+# sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+# ismlar = [] # bo'sh ro'yxat
+
+
+
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# print("Birinchi meva: ", mevalar[0])
+# print("Ikkinchi meva: ", mevalar[1])
+
+
+
+# mevalar = ['olma', 'anjir','shaftoli','o`rik']
+# print("birinchi meva: ",mevalar[0])
+# print("ikkinchi meva:",mevalar[1])
+# print("uchinchi meva:",mevalar[3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
